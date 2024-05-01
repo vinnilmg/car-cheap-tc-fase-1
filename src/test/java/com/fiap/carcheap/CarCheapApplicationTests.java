cd5badda9684b880a4a407886cbcc609563abafb
vinnilmg <vini.delimagomes@gmail.com>
2024-05-01 19:22:44 -0300

    feat: inicializando projeto

---
+++ src/test/java/com/fiap/carcheap/CarCheapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.carcheap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCheapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
