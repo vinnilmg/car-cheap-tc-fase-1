afaf754dc7e6d1d2e90898e612179f6d6ff8618c
Leonardo Padilha <leoprsouza@hotmail.com>
2024-05-13 17:27:52 -0300

    feat: definindo atributos cliente
    atributos basicos definidos para o cliente

--- src/main/java/com/fiap/carcheap/model/Cliente.java
+++ src/main/java/com/fiap/carcheap/model/Cliente.java
@@ -1,5 +1,34 @@
 package com.fiap.carcheap.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    @Size(min = 11, max = 11)
+    private String cpf;
+    @NotBlank
+    @Email
+    private String email;
+    @Size(min = 10, max = 10)
+    private String rg;
 
 }
