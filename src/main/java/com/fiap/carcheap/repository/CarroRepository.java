7f7cd66753ac88bbef30c0e962d00faaf2c9d9fa
vinnilmg <vini.delimagomes@gmail.com>
2024-05-25 16:18:15 -0300

    uuid fix

--- src/main/java/com/fiap/carcheap/repository/CarroRepository.java
+++ src/main/java/com/fiap/carcheap/repository/CarroRepository.java
@@ -2,8 +2,6 @@ package com.fiap.carcheap.repository;
 
 import com.fiap.carcheap.repository.entity.Carro;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CarroRepository extends JpaRepository<Carro,Long> {
+public interface CarroRepository extends JpaRepository<Carro, Long> {
 }
